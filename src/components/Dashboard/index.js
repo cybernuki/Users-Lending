507d2fc5a2240f38fcec696c0d3accd515b2d1de
danny099 <dalmariv01@gmail.com>
2020-06-10 09:51:10 -0500

    dashboard beta

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
-import { Button } from 'react-bootstrap';
-
+import { Card, ProgressBar, Container, Row, Col } from 'react-bootstrap';
+let now = 10
 const DashboardPage = () => (
   <>
-  <Button variant="primary">Primary</Button>{' '}
+  <Container>
+  <Row>
+    <Col md="auto">
+    <Card style={{ width: '30rem' }}>
+      <Card.Header>Cola</Card.Header>
+      <Card.Body>
+        <Card.Title>Numero en cola: 2</Card.Title>
+      </Card.Body>
+    </Card>
+    </Col>
+    <Col md="auto">
+    <Card style={{ width: '30rem' }}>
+      <Card.Header>Dinero reunido</Card.Header>
+      <Card.Body>
+        <Card.Title>Dinero reunido: {now}/3000000</Card.Title>
+        <ProgressBar now={now} label={`${now}%`} />
+      </Card.Body>
+    </Card>
+    </Col>
+  </Row>
+  </Container>
 </>
 )
 
