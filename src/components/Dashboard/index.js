70084a8ab2c058de7a57e020cfa8c53a601fc2f7
Feli <srgatoiscool@gmail.com>
2020-06-16 03:20:13 -0500

    added protect routes and system loggin

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card, ProgressBar, Container, Row, Col, CardDeck } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom'
 
 class DashboardPage extends React.Component {
   constructor(props) {
@@ -8,23 +9,49 @@ class DashboardPage extends React.Component {
     this.state = {
       amount: '',
       status: '',
+      redirect: false
     };
+  }
+
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    })
+  }
+
+  renderRedirect = () => {
+    return <Redirect to='/home' />
   }
  
   componentDidMount() {
-    fetch('https://userslending-backend.glitch.me/api/v1/aspirants/aspirant1')
+    
+    const token = localStorage.getItem('tokenId');
+    if (!token) {
+      this.setRedirect();
+    }
+
+    fetch(`https://userslending-backend.glitch.me/api/v1/aspirants/${token}`)
     .then((response) => {
       return response.json()
     })
     .then((aspirants) => {
+      
       this.setState({ 
         amount: aspirants.fund.amount,
         status: aspirants.fund.status
        })
+    }).catch(error => {
+      
+      this.setRedirect()
+      this.renderRedirect();
+      console.error(error)  
     })
   }
 
   render() {
+    if (this.state.redirect) {
+      return <Redirect to='/home' />
+    }
     return (
       <>
       <Container>
